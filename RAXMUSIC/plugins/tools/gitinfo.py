53a8eb7821d74455c1618827251ae91d12e7bdf2
Akshay Gangwar <freefire.akshaygangwar@gmail.com>
2024-01-19 22:50:56 +0530

    Update gitinfo.py

--- RAXMUSIC/plugins/tools/gitinfo.py
+++ RAXMUSIC/plugins/tools/gitinfo.py
@@ -1,7 +1,7 @@
 import asyncio, os, time, aiohttp
 import aiohttp
 from pyrogram import filters
-from DAXXMUSIC import app
+from RAXMUSIC import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 ###
