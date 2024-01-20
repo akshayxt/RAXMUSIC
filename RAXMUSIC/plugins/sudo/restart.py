b7a38cffaa93d4493ef8b16f39676a9f8235f8ec
Akshay Gangwar <freefire.akshaygangwar@gmail.com>
2024-01-20 12:23:49 +0530

    Update restart.py

--- RAXMUSIC/plugins/sudo/restart.py
+++ RAXMUSIC/plugins/sudo/restart.py
@@ -110,7 +110,7 @@ async def update_(client, message, _):
         exit()
 
 
-@app.on_message(filters.command(["restart"]) & SUDOERS)
+@app.on_message(filters.command(["mrestart"]) & SUDOERS)
 async def restart_(_, message):
     response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
     ac_chats = await get_active_chats()
