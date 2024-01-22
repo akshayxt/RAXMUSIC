c472204f2da6cdb15aebe2f95f5cad98bd73566c
Akshay Gangwar <130240533+akshayxt@users.noreply.github.com>
2024-01-22 22:21:05 +0530

    Update tagall.py

--- RAXMUSIC/plugins/admins/tagall.py
+++ RAXMUSIC/plugins/admins/tagall.py
@@ -6,15 +6,19 @@ from pyrogram.enums import ChatType, ChatMemberStatus
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import ChatPermissions
 
+import asyncio
+from RAXMUSIC import app
+from pyrogram import filters
+
 
 SPAM_CHATS = []
 
 
-@app.on_message(filters.command(["tagall", "all", "utag", "tag",]) | filters.command("@all", "") & filters.group)
+@app.on_message(filters.command(["tagall", "all" "utag"]) | filters.command("@all", "","utag") & filters.group)
 async def tag_all_users(_,message): 
     replied = message.reply_to_message  
     if len(message.command) < 2 and not replied:
-        await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ**") 
+        await message.reply_text("**𝚁𝙴𝙿𝙻𝚈 𝙰 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝚃𝙷𝙴𝙽 𝚄𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳 𝙾𝚁 𝚆𝚁𝙸𝚃𝙴 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 𝙼𝙴𝚂𝚂𝙰𝙶𝙴 𝙵𝙾𝚁 𝚃𝙰𝙶𝙶𝙸𝙽𝙶 𝙱𝚈 ||RAXMUSIC�スビ💔||**") 
         return                  
     if replied:
         SPAM_CHATS.append(message.chat.id)      
@@ -23,9 +27,9 @@ async def tag_all_users(_,message):
         async for m in app.get_chat_members(message.chat.id): 
             if message.chat.id not in SPAM_CHATS:
                 break       
-            usernum += 5
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 1:
+            usernum += 6
+            usertxt += f"\n✦ [{m.user.first_name}](tg://user?id={m.user.id}) ✦ , ✦ [{m.user.first_name}](tg://user?id={m.user.id}) ✦\n"
+            if usernum == 2:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(2)
                 usernum = 0
@@ -43,10 +47,10 @@ async def tag_all_users(_,message):
         async for m in app.get_chat_members(message.chat.id):       
             if message.chat.id not in SPAM_CHATS:
                 break 
-            usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
-            if usernum == 5:
-                await app.send_message(message.chat.id,f'{text}\n{usertxt}')
+            usernum += 2
+            usertxt += f"\n✦ [{m.user.first_name}](tg://user?id={m.user.id}) ✦ , ✦ [{m.user.first_name}](tg://user?id={m.user.id}) ✦\n"
+            if usernum == 6:
+                await app.send_message(message.chat.id,f'   ⤜⤜⤜ RAXMUSIC� 𝙼𝙸𝚂𝚂𝙸𝙽𝙶 ||スビ💔|| 🥺 ⤛⤛⤛\n\n🌔━━━━━━━━━━━━━━━━━━━━━🌔\n\n➜{text}\n\n🌔━━━━━━━━━━━━━━━━━━━━━🌔\n{usertxt}')
                 await asyncio.sleep(2)
                 usernum = 0
                 usertxt = ""                          
@@ -63,8 +67,8 @@ async def cancelcmd(_, message):
             SPAM_CHATS.remove(chat_id)
         except Exception:
             pass   
-        return await message.reply_text("ᴛᴀɢ ᴀʟʟ 🆂ᴜᴄᴄᴇ🆂🆂ғᴜʟʟʏ 🆂ᴛᴏᴘᴘᴇᴅ!")     
+        return await message.reply_text("**≕≕ 𝚃𝙰𝙶 𝙿𝚁𝙾𝙲𝙴𝚂𝚂 𝚂𝚃𝙾𝙿𝙴𝙳 𝙱𝚈 RAXMUSIC�||スビ💔|| ≔≔**")     
                                      
     else :
-        await message.reply_text("🇸​​🇴​​🇷​​🇷​​🇾​ ​🇨​​🇺​​🇷​​🇷​​🇪​​🇳​​🇹​​🇱​​🇾​ ​🇮​ ​🇦​​🇲​ ​🇳​​🇴​​🇹​ ​🇹​​🇦​​🇬​​🇮​​🇳​​🇬​ ​🇦​​🇳​​🇾​​🇴​​🇳​​🇪​")  
+        await message.reply_text("**◈ 𝙽𝙾 𝙾𝙿𝙴𝚁𝙰𝚃𝙸𝙾𝙽 𝙸𝚂 𝙶𝙾𝙸𝙽𝙶 𝙾𝙽 RAXMUSIC�||スビ💔|| ◈**")  
         return       
