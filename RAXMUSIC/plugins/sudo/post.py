398676dff62b8d6af0a4980cea21b08af1fcd3d7
Akshay Gangwar <130240533+akshayxt@users.noreply.github.com>
2024-02-05 13:11:32 +0530

    Update post.py

--- RAXMUSIC/plugins/sudo/post.py
+++ RAXMUSIC/plugins/sudo/post.py
@@ -9,7 +9,7 @@ async def copy_messages(_, message):
 
     if message.reply_to_message:
       
-        destination_group_id = -1002102454905
+        destination_group_id = -1002078269704
  
 
         
